d84fd12f4318a0b61589197eee6e77de3fae6b26
Remington Motte <127995162+remingtonrimo@users.noreply.github.com>
2024-07-18 14:12:27 -0500

    Add files via upload

---
+++ ancestral_character_states/contmap.R
@@ -0,0 +1,5 @@
+# I'm going to map the values from the chronogram (comparing DSCAM and mitochondrial
+# branch lengths) onto the DSCAM phylogeny, then subsequently map the ancestral
+# character state reconstruction on top of that in the hopes of visualizing 
+# the coordination of conservation/acceleration and social structure.
+library(phytools)
